9e9cf5a8dc94d12594f73a7dd73f94c7b038313a
isaacola10 <isaacolaolu10@gmail.com>
2021-02-28 10:53:34 +0100

    Fixed PORT

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb://localhost/merng",
+    MONGODB: "mongodb+srv://root:mata4life@cluster0.z8icx.mongodb.net/merng?retryWrites=true&w=majority",
     SECRET_KEY: "MerngSecret"
 }
